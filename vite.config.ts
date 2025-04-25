96c9aa43ae3f6e35e9e51b4c93409047a99f1e93
Kiril <kirilsboroviks3@gmail.com>
2025-04-25 20:16:35 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/dapp-ton/',
   plugins: [react()],
-  base: '/dapp-ton/', // Replace with your repository name
+   // Replace with your repository name
 });
