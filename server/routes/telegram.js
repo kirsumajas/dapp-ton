eda3d27ffdee873d0f8c32a16a5fe7b36fe19b52
Kiril <kirilsboroviks3@gmail.com>
2025-06-26 16:49:30 +0300

    telegram subscriptio check fixing 11

--- server/routes/telegram.js
+++ server/routes/telegram.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { verifySubscription } = require('../controllers/telegramController');
+const { isUserSubscribed } = require('../controllers/telegramController');
 
-router.post('/verify-subscription', verifySubscription);
+// Wrap helper in an Express handler
+router.post('/verify-subscription', async (req, res) => {
+  const { telegramId } = req.body;
+
+  if (!telegramId) {
+    return res.status(400).json({ success: false, message: 'Missing telegramId' });
+  }
+
+  const subscribed = await isUserSubscribed(telegramId);
+
+  if (subscribed) {
+    return res.json({ success: true, message: 'User is subscribed' });
+  } else {
+    return res.status(403).json({ success: false, message: 'User is not subscribed' });
+  }
+});
 
 module.exports = router;
