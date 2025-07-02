5d4cb0ef15f20990a7b888e292d6ddbdae1b9e94
Kiril <kirilsboroviks3@gmail.com>
2025-07-02 17:12:06 +0300

    Add get wallet

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -4,4 +4,12 @@ const walletController = require('../controllers/walletController');
 
 router.post('/connect', walletController.connectWallet);
 
+router.get('/all', (req, res) => {
+  const wallets = db.prepare(`
+    SELECT * FROM user_wallets
+  `).all();
+
+  res.json(wallets);
+});
+
 module.exports = router;
